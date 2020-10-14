02b5335f090eac32fba755f6f9e1a9545fbe9290
Jairo Avila <jairoavilam@gmail.com>
2020-10-14 09:23:51 -0500

    integrando section5

--- src/components/section5.js
+++ src/components/section5.js
@@ -4,25 +4,25 @@ import Bill from '../assets/img/Bill.jpg'
 const section5 = () => {
     return (
         <div>
-           <div class="container">
-      <section class="container">
-          <div class="container d-flex  justify-content-center">
+           <div className="container">
+      <section className="container">
+          <div className="container d-flex  justify-content-center">
               <h4>OUR TESTIMONIAL</h4>
             </div>
-         <div  class="row py-5 mx-5 ">
+         <div  className="row py-5 mx-5 ">
             <div id="Testimonial" class="col col-12 col-md-4 col-lg-4 col-xl-4 ">
-              <div class="container d-flex justify-content-end">
+              <div className="container d-flex justify-content-end">
                 <img src={Bill}alt=""/>
               </div>
             </div>
-          <div class="col col-12 col-md-8 col-lg-8 col-xl-8 ">
-             <div class="container font-weight-light border-left "  >
+          <div className="col col-12 col-md-8 col-lg-8 col-xl-8 ">
+             <div className="container font-weight-light border-left "  >
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident natus facilis sint itaque minus numquam nemo nobis excepturi! Eligendi, in architecto labore ducimus facilis commodi, voluptate aliquam mollitia porro vitae!</p>
               </div>
-              <div class="container pt-3">
+              <div className="container pt-3">
                 <h4>Bill Gates</h4>
               </div>
-              <div class="container">
+              <div className="container">
                 <h6>Microsoft CEO</h6>
               </div>
             </div>
